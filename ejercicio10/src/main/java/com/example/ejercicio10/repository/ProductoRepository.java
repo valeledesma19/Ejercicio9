c01c5189d5f0269c71421682c3aa18e3d7262cfa
valeledesma19 <valentinaledesma2407@gmail.com>
2026-04-28 11:26:13 -0300

    Delete ejercicio10 directory

--- ejercicio10/src/main/java/com/example/ejercicio10/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ejercicio10.repository;
-
-import com.example.ejercicio10.models.Producto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
-}
